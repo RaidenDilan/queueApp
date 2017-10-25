3659c535836fcbb329ccec3725700520d738eb04
Raiden Dilan <raiden18@me.com>
2017-10-25 15:11:55 +0100

    added submissions routes to config/routes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 const auth = require('../controllers/auth');
 const users = require('../controllers/users');
+const submissions = require('../controllers/submissions');
 const secureRoute = require('../lib/secureRoute');
+
+router.route('/submissions')
+  .all(secureRoute)
+  .get(submissions.index)
+  .post(submissions.create);
+
+router.route('/submissions/:id')
+  .all(secureRoute)
+  .get(submissions.show);
 
 router.route('/users')
   .all(secureRoute)
